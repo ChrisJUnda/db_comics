26fa3d5e8a07ca39e7ebb1b27071f632a602a02d
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-15 14:31:49 +0200

    [UPD] Created Scaffholding of Header and Footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    <h1>
-        Prova
-    </h1>
-
-    <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template">
-
-</body>
-
-</html>
+@extends('layouts.app')
+@section('main')
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h1>
+                    The Comics
+                </h1>
+            </div>
+        </div>
+    </div>
+@endsection
